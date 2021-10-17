92ac28adce7331a9a7c5b8b663a24488a1d2f791
ALOGOM <60058140+logologologo000@users.noreply.github.com>
2021-10-17 23:38:30 +0700

    m
    m

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const { response } = require('express');
 const mysql = require('mysql');
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
-const uuid = require('uuid')
+const uuid = require('uuid');
+const { send } = require('process');
+const fs = require('fs');
 const app = express();
 
 
@@ -33,12 +35,54 @@ app.use(cookieSession({
 
 ///////////////////////////////////////////////// REQUESTS //////////////////////////////////////////
 
+//set notice in req
+app.post('/setnotice', (req, res) => {
+    const notice = req.body.notice
+    const request_id = req.body.request_id
+    connection.execute('UPDATE requests SET notice = ?, status = 2 WHERE request_id = ?', [notice, request_id])
+    .then(() => {
+        
+    })
+})
+
+
+// Set req status
+app.post('/setstatusreq', (req, res) => {
+    const request_id = req.body.request_id
+    const status = req.body.status
+    console.log(status)
+    console.log(request_id)
+       connection.execute('UPDATE requests SET status = ? WHERE request_id = ?', [status, request_id])
+        .then(() => {
+            res.status(200).send('Success').end()
+        }) 
+    
+
+
+
+    
+
+})
+
 // Delete req with request_id
 app.delete('/deletereq/:request_id', (req, res) => {
     const request_id = req.params.request_id
+
+
+    connection.execute('SELECT img FROM requests WHERE request_id = ?', [request_id])
+        .then(([result]) => {
+            console.log(result[0].img)
+            fs.unlink(`${__dirname}/../client/public/uploads/${result[0].img}`, () => {console.log("Delete")})
+        })
+
     connection.execute('DELETE FROM requests WHERE request_id = ?', [request_id])
     .then(() => {
+        
         res.status(200).send('Delete Success')
+
+        
+
+
     })
 })
 
@@ -77,6 +121,13 @@ app.post('/editreqimg', (req, res) => {
     const port = req.body.port
     const request_id = req.body.request_id
     file.name = uuid.v4()+".jpg"
+
+    connection.execute('SELECT img FROM requests WHERE request_id = ?', [request_id])
+        .then(([result]) => {
+            console.log(result[0].img)
+            fs.unlink(`${__dirname}/../client/public/uploads/${result[0].img}`, () => {console.log("Delete")})
+        })
+
     
 
     file.mv(`${__dirname}/../client/public/uploads/${file.name}`, err => {
@@ -124,12 +175,70 @@ app.post('/createreq', (req, res) => {
     
 })
 
+//get all reqs
+app.get('/getallreqs', (req, res) => {
+    connection.execute('SELECT * FROM requests').then(([result]) => {
+        console.log('Get all is done')
+        
+        res.status(200).json(result).end()
+        
 
-//get all reqs from user ID
+    })
+})
+
+//get all reqs with status 0
+app.get('/getreqsstatus0', (req, res) => {
+    connection.execute('SELECT * FROM requests where status = 0').then(([result]) => {
+        console.log('Get all is done')
+        
+        res.status(200).json(result).end()
+        
+
+    })
+})
+
+//get all reqs with status 1
+app.get('/getreqsstatus1', (req, res) => {
+    connection.execute('SELECT * FROM requests where status = 1').then(([result]) => {
+        console.log('Get all is done')
+        
+        res.status(200).json(result).end()
+        
+
+    })
+})
+
+
+//get all reqs with status 2
+app.get('/getreqsstatus2', (req, res) => {
+    connection.execute('SELECT * FROM requests where status = 2').then(([result]) => {
+        console.log('Get all is done')
+        
+        res.status(200).json(result).end()
+        
+
+    })
+})
+
+
+//get all reqs with status 0 and 1
+app.get('/getreqsstatus', (req, res) => {
+    connection.execute('SELECT * FROM requests WHERE status IN (  0 , 1 ) ').then(([result]) => {
+        console.log('Get all is done')
+        
+        res.status(200).json(result).end()
+        
+
+    })
+})
+
+
+
+//get reqs by user ID
 app.post('/getreqs', (req, res) => {
     const user_id = req.body.user_id
     if(user_id){
-      connection.execute('SELECT * FROM requests WHERE user_id = ?', [user_id])
+      connection.execute('SELECT * FROM requests WHERE user_id = ? ORDER BY request_id DESC', [user_id])
         .then(([result]) => {
             res.json(result)
             res.end
@@ -140,7 +249,7 @@ app.post('/getreqs', (req, res) => {
     
 })
 
-//get a req from req_ID
+//get a req by req_ID
 app.post('/getreq', (req, res) => {
     const req_id = req.body.request_id
     if(req_id){
@@ -174,7 +283,7 @@ app.post('/register', [
             return true
         })
     }),
-    body('username', 'invalid username').trim().custom((value) => {
+    body('username', 'invalid username 6 - 12 characters').trim().isLength({ min:6 , max:12}).custom((value) => {
         return connection.execute('SELECT username FROM users WHERE username = ?', [value])
         .then(([rows]) => {
             if (rows.length > 0) {
@@ -185,7 +294,7 @@ app.post('/register', [
             return true
         })
     }),
-    body('code', 'invalid username').trim().custom((value) => {
+    body('code', 'invalid code').trim().custom((value) => {
         return connection.execute('SELECT code FROM users WHERE code = ?', [value])
         .then(([rows]) => {
             if (rows.length > 0) {
@@ -194,7 +303,7 @@ app.post('/register', [
             return true
         })
     }),
-    body('password', 'invalid password').trim().isLength({ min : 6})
+    body('password', 'invalid password min 6 characters ').trim().isLength({ min : 6})
 ], (req, res, next) => {
     const validation_result = validationResult(req)
     const { username, email ,password, code , firstname, lastname , subject } = req.body
@@ -262,9 +371,10 @@ app.post('/login', [
         .then(([rows]) => {
             
             bcrypt.compare(password, rows[0].password).then(compare_result => {
-                
+                // console.log(rows[0].password)
+                // console.log(password)
+                // console.log(compare_result)
                 if (compare_result === true) {
-                    
                     req.session.isLoggedIn = true;
                     req.session.id = rows[0].id
                     req.session.status = rows[0].level
@@ -304,6 +414,32 @@ app.get('/logout', (req, res) => {
 
 ///////////////////////////////////////// USERS //////////////////////////////////////////
 
+//set user level
+app.put('/setuserlevel', (req, res) => {
+    const { user_id , level } = req.body
+    connection.execute('UPDATE users SET level = ?  WHERE user_id = ?', [level , user_id])
+    .then(() => {
+        res.status(200).send('Success').end()
+    })
+})
+
+
+
+//Gat all TA
+app.get('/getallta', (req, res) => {
+    connection.execute('SELECT * FROM users where level = 1').then(([result]) => {
+        res.status(200).send(result).end()
+    })
+})
+
+//Get all users
+app.get('/getallusers', (req, res) => {
+    connection.execute('SELECT * FROM users WHERE level != 2').then(([result]) => {
+        
+        res.status(200).send(result).end()
+    })
+})
+
 // Get user by user_id
 app.get('/getuser/:user_id', (req, res) => {
     console.log('Start')
@@ -320,37 +456,56 @@ app.get('/getuser/:user_id', (req, res) => {
 
 //Edit user by user_id
 app.post('/edituser/:user_id', [
-    body('username', 'invalid username').trim().custom((value) => {
+    body('username', 'invalid username').trim().custom((value, {req}) => {
 
-    return connection.execute('SELECT username FROM users WHERE username = ?', [value])
-    .then(([rows]) => {
-        if (rows.length > 0) {
-            console.log('This username already exsit')
-            return Promise.reject('This username already exsit')
-        }
+        return connection.execute('SELECT username FROM users WHERE username = ?', [value])
+        .then(([rows]) => {
+            if (rows.length > 0) {
+                if (rows[0].username != req.body.old_username) {
+                    console.log('This username already exsit')
+                    return Promise.reject('This username already exsit')
+                }
+                
+            }
         
     })
-    })] 
+    }),
+    body('code', 'invalid code').trim().custom((value, {req}) => {
+
+        return connection.execute('SELECT code FROM users WHERE code = ?', [value])
+        .then(([rows]) => {
+            if (rows.length > 0) {
+                if (rows[0].code != req.body.old_code) { 
+                    console.log('This code already exsit')
+                    return Promise.reject('This code already exsit')
+                }
+            }
+            
+        })
+    }),
+    body('email', 'invalid email').trim().custom((value, {req}) => {
+
+        return connection.execute('SELECT email FROM users WHERE email = ?', [value])
+        .then(([rows]) => {
+            if (rows.length > 0) {
+                if (rows[0].email != req.body.old_email) {
+                    console.log('This email already exsit')
+                    return Promise.reject('This email already exsit')
+                }
+            }
+            
+        })
+        })
+    ]
     ,(req, res) =>{
         console.log('Start')
         const user_id = req.params.user_id
-        const old_username = req.body.old_username
         const username = req.body.username
         const firstname = req.body.firstname
         const lastname = req.body.lastname
         const email = req.body.email
         const subject = req.body.subject
         const code = req.body.code
-
-        console.log({
-        old_username,
-        username,
-        firstname,
-        lastname,
-        email,
-        subject,
-        code
-        })
 
         const validation_result = validationResult(req)
         console.log(validation_result.isEmpty())
@@ -368,33 +523,170 @@ app.post('/edituser/:user_id', [
 
         } else {
 
-        console.log(req.body.username == old_username)
-            if (req.body.username == old_username) {
-                
-                connection.execute('UPDATE users SET username = ?, firstname = ?, lastname = ?, email = ? , subject = ? ,code = ? WHERE user_id = ?', [ username , firstname, lastname, email, subject, code , user_id])
-                .then((result) => {
-                    
-                    console.log('End')
-                    
-                    res.status(200).send('Success to Edit').end()
+                let allErrors = validation_result.errors.map((error) => {
+                    return error.msg
                 })
-
-
-            } else {
-                        
-                        let allErrors = validation_result.errors.map((error) => {
-                            return error.msg
-                        })
-                        return res.send({
-                            process_errors: allErrors
-                        })
-
-            }
-
-            
+                return res.send(allErrors).end()
+        
         }
             
     })
+
+    // RESET user password by user_id
+    app.put('/resetpassword/:user_id', (req, res) => {
+        const user_id = req.params.user_id
+        const old_password = req.body.old_password
+        const new_password = req.body.new_password
+        console.log(old_password) 
+        console.log(new_password) 
+
+        bcrypt.compare( new_password, old_password).then(compare_result => {
+            console.log(compare_result)
+            
+            if(!compare_result){
+                bcrypt.hash(new_password, 12).then((hash) => {
+
+                    connection.execute('UPDATE users SET password = ? WHERE user_id = ?', 
+                    [hash, user_id])
+                    .then(() => {
+                        console.log('Success')
+                        return res.status(200).send('Success').end()
+                    })
+
+                })
+                        
+
+            } else {
+
+                console.log('Do nothing')
+                return res.status(200).send('Do nothing').end()
+            }
+
+        })
+
+        
+    })
+
+    // Delete user by user_id
+    app.delete('/deleteuser/:user_id', (req, res) => {
+        const user_id = req.params.user_id
+        connection.execute('DELETE FROM users WHERE user_id = ?', [user_id]).then(() => {
+            res.status(200).send('Success').end()
+        })
+
+    })
+
+
+
+
+////////////////////////////////////////////////NOTICES /////////////////////////////////////
+
+//edit notice with img
+app.post('/editnoticeimg/:notice_id', (req, res) => {
+
+    if(req.files === null){
+        return res.status(400).json({ msg: 'no file upload'})
+    }
+
+    const notice_id = req.params.notice_id
+
+    connection.execute('SELECT img FROM notices WHERE notice_id = ?', [notice_id])
+        .then(([result]) => {
+            console.log(result[0].img)
+            fs.unlink(`${__dirname}/../client/public/uploads/notice/${result[0].img}`, () => {console.log("Delete")})
+        })
+
+    const file = req.files.file
+    const title = req.body.title
+    const detail = req.body.detail
+    
+    file.name = uuid.v4()+".jpg"
+
+    file.mv(`${__dirname}/../client/public/uploads/notice/${file.name}`, err => {
+        if(err) {
+            console.error(err)
+            return res.status(500).send(err)
+        }
+        res.json({ fileName: file.name, filePath: `/uploads/notice/${file.name}`})
+    })
+    
+    connection.execute('UPDATE notices SET img = ?, title = ?, detail = ? WHERE notice_id = ?',
+        [file.name, title ,detail ,notice_id]).catch(err => {
+         console.error(err)
+         
+     })
+
+})
+
+//edit notice without img
+app.post('/editnotice/:notice_id', (req, res) => {
+    const notice_id = req.params.notice_id
+    const title = req.body.title
+    const detail = req.body.detail
+    connection.execute('UPDATE notices SET title = ?, detail = ? WHERE notice_id = ?', 
+    [title, detail, notice_id]).then(() => {
+
+        res.status(200).send({msg:'Success to Edit'}).end()
+    }).catch((error) => {
+        console.log(error)
+    })
+
+})
+
+
+//get Notice by id
+app.get('/getnotice/:notice_id', (req, res) => {
+    const notice_id = req.params.notice_id
+    connection.execute('SELECT * FROM notices WHERE notice_id = ?', [ notice_id])
+    .then(([result]) => {
+        res.status(200).json(result).end()
+    })
+})
+
+//Get all Notices 
+app.get('/getallnotices', (req, res) => {
+    connection.execute('SELECT * FROM notices').then(([response]) => {
+        return res.json(response)
+    })
+})
+
+//create notice
+app.post('/createnotice', (req, res) => {  
+
+    if(req.files === null){
+        return res.status(400).json({ msg: 'no file upload'})
+    }
+    const detail = req.body.detail
+    const title = req.body.title
+    const file = req.files.file
+
+    file.name = uuid.v4()+".jpg"
+    
+    file.mv(`${__dirname}/../client/public/uploads/notice/${file.name}`, err => {
+        if(err) {
+            console.error(err)
+            return res.status(500).send(err)
+        }
+        res.json({ fileName: file.name, filePath: `/uploads/notice/${file.name}`})
+    })
+    connection.execute('INSERT INTO notices (title , img , detail ) VALUES ( ?, ?, ?)',
+     [title, file.name ,detail])
+    
+})
+
+//delete notice by notice_id
+app.delete('/deletenotice/:notice_id', (req, res) => {
+    const notice_id = req.params.notice_id
+
+    connection.execute('SELECT img FROM notices WHERE notice_id = ?', [notice_id])
+        .then(([result]) => {
+            console.log(result[0].img)
+            fs.unlink(`${__dirname}/../client/public/uploads/notice/${result[0].img}`, () => {console.log("Delete")})
+        })
+    connection.execute('DELETE FROM notices WHERE notice_id = ?', [notice_id]).then(() => {
+        res.status(200).send('Success').end()
+    })
+})
 
 
 
